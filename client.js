3bce73343918b4a5f6d768d2a6e4bde7509227b4
Aleksandr.Gudenko <aleksandr.gudenko@together.com>
2018-08-13 11:15:01 +0300

    upd

--- client.js
+++ client.js
@@ -1,25 +1,48 @@
-$(document).ready(function() {
-  var socket = new WebSocket("ws://127.0.0.1:1337");
+const http = require("http");
 
-  socket.onopen = function() {
-    console.log("Соединение установлено.");
-  };
+const PORT = 8888;
 
-  socket.onclose = function(event) {
-    if (event.wasClean) {
-      console.log('Соединение закрыто чисто');
-    } else {
-      console.log('Обрыв соединения'); // например, "убит" процесс сервера
-    }
-    console.log('Код: ' + event.code + ' причина: ' + event.reason);
-  };
+const js = `
+    var socket = new WebSocket("ws://127.0.0.1:1337");
 
-  socket.onmessage = function(event) {
-    console.log("Получены данные " + event.data);
-  };
+    socket.onopen = function() {
+      console.log("Соединение установлено.");
+    };
+  
+    socket.onclose = function(event) {
+      if (event.wasClean) {
+        console.log('Соединение закрыто чисто');
+      } else {
+        console.log('Обрыв соединения'); // например, "убит" процесс сервера
+      }
+      console.log('Код: ' + event.code + ' причина: ' + event.reason);
+    };
+  
+    socket.onmessage = function(event) {
+      console.log("Получены данные " + event.data);
+    };
+  
+    socket.onerror = function(error) {
+      console.log("Ошибка " + error.message);
+    };
+`;
 
-  socket.onerror = function(error) {
-    console.log("Ошибка " + error.message);
-  };
+new http.Server(function (req, res) {
+  res.end(
+    `
+      <!doctype html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>Test Websocket</title>
+        </head>
+        <body>
+        </body>
+        <script>
+          ${js}
+        </script>
+      </html>
+    `
+  );
+}).listen(PORT);
 
-});
